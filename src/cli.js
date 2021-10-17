37b2719a5ad861efb4f0f57bc3fdeaa604331453
Анна <anna_s92_@mail.ru>
2021-10-17 14:53:07 +0400

    add game even

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 export default welcome;
