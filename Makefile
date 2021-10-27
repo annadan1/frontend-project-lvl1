693e4da4055f7d7d28b455c3aebf1acf8126f6e4
Анна <anna_s92_@mail.ru>
2021-10-27 17:10:19 +0400

    add brain-gcd game

--- Makefile
+++ Makefile
@@ -10,4 +10,6 @@ brain-even:
 	node bin/brain-even.js
 brain-calc:
 	node bin/brain-calc.js
+brain-gcd:
+	node bin/brain-gcd.js
 
---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import game from '../src/games/gcd.js';
+import play from '../src/index.js';
+
+play(game);
