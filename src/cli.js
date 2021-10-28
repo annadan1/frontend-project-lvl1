c934fd82d30b13c42c87b0d46601f78865570af1
Анна <anna_s92_@mail.ru>
2021-10-28 18:55:46 +0400

    src files changed

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const play = (game) => {
-console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
@@ -15,13 +15,13 @@ console.log('Welcome to the Brain Games!');
     const answer = readlineSync.question('Your answer: ');
 
     if (answer !== correctAnswer) {
-        console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-        console.log(`Let's try again, ${userName}!`);
-        return;
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
     console.log('Correct!');
-}
-console.log(`Congratulations, ${userName}!`);
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default play;
